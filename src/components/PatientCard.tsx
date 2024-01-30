db6e1fb71a69dfb5dbee1b3be8c4d5b36b83202c
shahrukhshaikh19 <shaikhshahrukh078@gmail.com>
2024-01-30 11:28:46 +0530

    patients data fetched

--- src/components/PatientCard.tsx
+++ src/components/PatientCard.tsx
@@ -13,15 +13,19 @@ interface Patient {
 }
 
 const PatientCard: React.FC<Patient> = ({ name, id, age, address, avatar }) => {
-  return (
-    <div className="max-w-md mx-auto bg-white rounded-lg shadow-md p-6 hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
+   return (
+    <div className="w-full bg-[#0369a1] rounded-lg shadow-md p-6 hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
       <div className="flex items-center">
-        <img src={`https://robohash.org/${id}?200x200`} alt="Patient Avatar" className="w-12 h-12 rounded-full mr-4 border-2 border-black" />
+        <img
+          src={`https://robohash.org/${id}?200x200`}
+          alt="Patient Avatar"
+          className="w-12 h-12 rounded-full mr-4 border-2 border-black"
+        />
         <div>
-          <h2 className="text-lg font-bold text-gray-600">{name}</h2>
-          <p className="text-gray-600">ID: {id}</p>
-          <p className="text-gray-600">Age: {age}</p>
-          <p className="text-gray-600">Address: {address}</p>
+          <h2 className="text-lg font-bold text-white">{name}</h2>
+          <p className="text-gray-200">ID: {id}</p>
+          <p className="text-gray-200">Age: {age}</p>
+          <p className="text-gray-200">Address: {address}</p>
         </div>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,122 +1,34 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import PatientCard from '../components/PatientCard';
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const patients = [
-    {
-      id: '1',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '2',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '3',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '4',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '5',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '6',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '7',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '8',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '9',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '10',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '11',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '12',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '13',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '14',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    {
-      id: '15',
-      name: 'John Doe',
-      age: 35,
-      address: '123 Main St, Anytown',
-      avatar: 'https://via.placeholder.com/150',
-    },
-    // Add more patient data as needed
-    // ...
-  ];
+  const [patientsData,setPatientssData] = useState([]);
+  const apiUrl = "http://localhost:5000";
+  useEffect(()=>{
+    const fetchPatientsData=async()=>{
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error('Failed to fetch user data');
+        }
+        const totalPatients = await  response.json();
+        setPatientssData(totalPatients.patients)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchPatientsData();
+  },[]);
+
+
+  
   return (
     <div className="container mx-auto py-8 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {patients.map(patient => (
+      {patientsData?.map(patient => (
         <PatientCard key={patient.id} {...patient} />
       ))}
     </div>
